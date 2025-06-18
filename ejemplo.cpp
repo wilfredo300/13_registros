323c0c8382148c602de8c3387769bda1178fc22a
wilfredo300 <junior.val6@gmail.com>
2025-06-18 18:18:32 -0500

    Se agrega la funcionalidad para mostrar al empleado con mas ventas en el año

--- ejemplo.cpp
+++ ejemplo.cpp
@@ -6,11 +6,12 @@ struct EMP{
     string nom;
     float ven[12];
     float sal;
-    float t_ven;
+    float t_ven; //venta total anual
 };
 int main(){
     EMP EMPLEADOS[100];
     int n,i,j;
+    int max(0);//indicador de mayor ventas de ventas
     cout<<"Ingresa el numero de empleados a ingresar: \n";
     cin>>n;
     for (i=0;i<n;i++){
@@ -20,25 +21,32 @@ int main(){
         cin>>EMPLEADOS[i].nom;
         cout<<"Ingresa el las ventas de empleado: ";
         EMPLEADOS[i].t_ven=0;
-        for(j=0;j<12;j++){
+        for(j=0;j<3;j++){
             cin>>EMPLEADOS[i].ven[j]; //registra la cantidad de ventas por mes
             EMPLEADOS[i].t_ven += EMPLEADOS[i].ven[j]; //suma el total de ventas al año por cada empleado
         }
         cout<<"Ingresa el salario del empleado\n";
         cin>>EMPLEADOS[i].sal;        
     }
+    //el empleado con mas ventas
+    for(i=0;i<n-1;i++){
+        if(EMPLEADOS[i].t_ven<EMPLEADOS[i+1].t_ven){
+            max=i+1;
+        }
+    }
     
-
-    /*
+    /*//  Esta seccion es solo para pruebas
     for (i=0;i<n;i++){
         cout<<"Empleado "<<i;
         cout<<EMPLEADOS[i].num<<" ";
-        cout<<EMPLEADOS[i].nom<<" ";
-        for(j=0;j<12;j++){
-            cout<<EMPLEADOS[i].ven[j]<<" ";
-        }
+        cout<<EMPLEADOS[i].nom<<" \nVentas totales:";
+        cout<<EMPLEADOS[i].t_ven<<" ";
         cout<<EMPLEADOS[i].sal<<endl;
     }
     */
+    
+    cout<<"El empleado con mas ventas es: "<<EMPLEADOS[max].nom<<" \n";
+    cout<<"Ventas totales: "<<EMPLEADOS[max].t_ven<<" \n";
+
     return 0;
 }
